817f76fe7ef87177711863c920819cf6b87a12a0
dlwhitehurst <dlwhitehurst@me.com>
2016-01-15 20:44:37 -0500

    initial check-in

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ciwiseTimekeeperApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ciwiseTimekeeperApp
+ */
+angular.module('ciwiseTimekeeperApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
